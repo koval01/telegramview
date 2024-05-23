32219c18987bdde2f4d6b425427b80ff59c6ab67
Koval Yaroslav <yaroslav@koval.page>
2024-05-23 13:41:58 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import Framework7 from 'framework7/lite/bundle'
+import Framework7React from 'framework7-react'
+import App from './App.tsx'
+
+import 'framework7/css/bundle'
+import './index.css'
+
+Framework7.use(Framework7React)
+
+createRoot(document.getElementById('root')!)
+    .render(React.createElement(App))
