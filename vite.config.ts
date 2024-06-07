8425ac1df7a3fcafe1db420a1d0f406714ddb0ea
Koval Yaroslav <yaroslav@koval.page>
2024-06-07 11:10:18 +0200

    Update chunkSplitPlugin config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 export default defineConfig({
   plugins: [
       react(),
-      chunkSplitPlugin(),
+      chunkSplitPlugin({
+        strategy: 'unbundle'
+      }),
       VitePWA({
         registerType: 'autoUpdate',
         injectRegister: 'inline',
