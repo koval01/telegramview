5e826571325f879f188bade0c7764cd5cbdd7c1f
Koval Yaroslav <yaroslav@koval.page>
2024-06-11 20:20:47 +0200

    Update Feed

--- src/pages/Feed/components/Post.tsx
+++ src/pages/Feed/components/Post.tsx
@@ -88,9 +88,9 @@ export const PollFragment = ({poll}: { poll: Poll | undefined }) => (
 );
 
 export const PostStats = ({ footer, poll }: { footer: Footer, poll?: Poll | undefined }) => (
-    <div className="relative flex flex-row justify-end items-end">
+    <div className="relative flex flex-row justify-end items-end right-0 w-full">
         {footer.views && (
-            <div className="flex gap-2 text-neutral-400">
+            <div className="flex gap-2">
                 {poll && (
                     <div className="flex gap-1">
                         <div>
@@ -114,7 +114,22 @@ export const PostStats = ({ footer, poll }: { footer: Footer, poll?: Poll | unde
     </div>
 );
 
-export const PostHead = ({ channel, post }: { channel: Channel, post: Post }) => (
+export const PostAuthor = ({ footer }: { footer: Footer }) => (
+    <div className="relative flex flex-row justify-start items-end">
+        {footer.author && (
+            <div className="flex gap-1">
+                <div>
+                    <Icon f7="person" size="14px" className="!align-baseline"/>
+                </div>
+                <div className="text-sm w-full whitespace-nowrap">
+                    {footer.author}
+                </div>
+            </div>
+        )}
+    </div>
+);
+
+export const PostHead = ({channel, post}: { channel: Channel, post: Post }) => (
     <React.Fragment>
         <div>
             <div
@@ -124,9 +139,11 @@ export const PostHead = ({ channel, post }: { channel: Channel, post: Post }) =>
         </div>
         <div>
             <div className="font-bold text-white flex gap-1">
-                <div>{channel.title}</div>
+                <div className="whitespace-nowrap">
+                    {channel.title}
+                </div>
                 <div>
-                    <Verified verified={!!channel.labels && channel.labels.includes("verified")}/>
+                <Verified verified={!!channel.labels && channel.labels.includes("verified")}/>
                 </div>
             </div>
             <div className="font-light text-sm text-neutral-400">
@@ -151,7 +168,8 @@ export const PostFragment: React.FC<{ channel: Channel; post: Post }> = ({ chann
                 }
                 <PollFragment poll={post.content?.poll} />
             </div>
-            <div className="mt-3 h-4 w-full select-none">
+            <div className="flex mt-3 h-4 w-full select-none text-neutral-400">
+                <PostAuthor footer={post.footer} />
                 <PostStats footer={post.footer} poll={post.content?.poll} />
             </div>
         </div>
