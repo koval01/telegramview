273e964f9c129e5edb983c6e1e4b2ecaa2343651
Koval Yaroslav <yaroslav@koval.page>
2024-06-13 00:42:05 +0200

    Update

--- src/pages/Feed/components/post/PostMedia.tsx
+++ src/pages/Feed/components/post/PostMedia.tsx
@@ -23,16 +23,19 @@ export const PostMedia = ({ post }: { post: Post }) => (
                 )}
                 {['roundvideo', 'video', 'gif', 'sticker'].some(type => media.type.includes(type)) && (
                     <div className={
-                        `w-10/12 md:w-3/5 m-auto relative ${post.content?.media?.length === 1 ? 'p-0' : 'py-10'}`
+                        `w-10/12 md:w-3/5 m-auto relative ${post.content?.media?.length === 1 ? 'p-0' : 'pb-8'}`
                     }>
-                        <div className={
-                            "fixed left-0 w-full h-full bg-center blur-md"
-                        } style={{
-                            backgroundImage: `url(${media.thumb})`,
-                            backgroundSize: "120%"
-                        }}></div>
-                        <div className="fixed left-0 w-full h-full bg-black opacity-30"></div>
-                        <div className="py-14">
+                        <div className="hidden md:block">
+                            <div className={
+                                "fixed left-0 w-full h-full bg-center blur-md"
+                            } style={{
+                                backgroundImage: `url(${media.thumb})`,
+                                backgroundSize: "120%"
+                            }}></div>
+                            <div className="fixed left-0 w-full h-full bg-black opacity-30"></div>
+                        </div>
+
+                        <div className="py-2 md:py-14">
                              <PostVideo
                                 url={media.url} thumb={media.thumb}
                                 roundvideo={media.type === 'roundvideo'}
